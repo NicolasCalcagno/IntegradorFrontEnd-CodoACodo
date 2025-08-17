56266addd9f14c73ef44e1f53dc3487a7b26fe54
NicolasCalcagno <87141678+NicolasCalcagno@users.noreply.github.com>
2025-08-17 18:51:12 -0300

    build: actualizacion conexion-bd.php para despliegue

--- backend/conexion-bd.php
+++ backend/conexion-bd.php
@@ -2,39 +2,10 @@
 define('BASE_PATH', dirname(__DIR__));
 include_once(BASE_PATH . '/config.php');
 
-function LoadEnv($path) {
-    if (!file_exists($path)) {
-        throw new Exception(".env file not found at $path");
-    }
-
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        $line = trim($line);
-
-        // Skip comments
-        if ($line === '' || str_starts_with($line, '#')) {
-            continue;
-        }
-
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-
-        // Remove optional surrounding quotes
-        $value = trim($value, '"\'');
-
-        // Set the environment variable
-        $_ENV[$name] = $value;
-        putenv("$name=$value");
-    }
-}
-
-LoadEnv(__DIR__ . '/../secrets/.env');
-
-$hostname = $_ENV['DB_HOST'];
-$username = $_ENV['DB_USER_PUBLIC'];
-$password = $_ENV['DB_PASS_PUBLIC'];
-$databaseName = $_ENV['DB_NAME_CONFERENCIA_BUENOS_AIRES'];
+$hostname = getenv('DB_HOST');
+$username = getenv('DB_USER_PUBLIC_NAME');
+$password = getenv('DB_USER_PUBLIC_PASS');
+$databaseName = getenv('DB_NAME_CONFERENCIA_BUENOS_AIRES');
 
 $conexion = mysqli_connect($hostname, $username, $password, $databaseName);
 
